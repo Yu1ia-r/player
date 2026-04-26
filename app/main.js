c9a721d7c511d77ac9a0d67f865b53c2a08fdc27
Yulia Rogulina <rogulina.yulia@gmail.com>
2026-04-26 13:33:46 +0300

    иконка паузы и плея на кнопке воспроизведения

--- app/main.js
+++ app/main.js
@@ -4,7 +4,10 @@ const startPlay = document.querySelector('.button-play')
 startPlay.addEventListener('click', () => {
   if(audio.paused) {
     audio.play()
+    startPlay.classList.add('is-playing')
   } else {
     audio.pause()
+    startPlay.classList.remove('is-playing')
+
   }
 })
