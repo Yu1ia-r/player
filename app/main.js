86f874fd3e3a2c56d97d15cdab46e636c3f93a70
Yulia Rogulina <rogulina.yulia@gmail.com>
2026-04-26 14:57:17 +0300

    отображение времени и трек бар

--- app/main.js
+++ app/main.js
@@ -1,8 +1,11 @@
 const audio = document.querySelector('.audio')
 const startPlay = document.querySelector('.button-play')
+const spanAudioDuration = document.querySelector('.end-time')
+const spanAudioCurrTime = document.querySelector('.current-time')
+const progressBar = document.querySelector('.progress-bar')
 
 startPlay.addEventListener('click', () => {
-  if(audio.paused) {
+  if (audio.paused) {
     audio.play()
     startPlay.classList.add('is-playing')
   } else {
@@ -11,3 +14,34 @@ startPlay.addEventListener('click', () => {
 
   }
 })
+
+audio.addEventListener('loadedmetadata', () => {
+  const audioDuration = audio.duration
+  const audioDuratinNornalize = `${Math.floor(audio.duration / 60)}:${Math.floor(audio.duration % 60)}`
+  spanAudioDuration.innerHTML = audioDuratinNornalize
+
+  progressBar.max = audioDuration
+
+})
+
+audio.addEventListener('timeupdate', () => {
+  const sec = (Math.floor(audio.currentTime % 60)).toString().length > 1 ? Math.floor(audio.currentTime % 60) : `0${Math.floor(audio.currentTime % 60)}`
+  const time = `${Math.floor(audio.currentTime / 60)}:${sec}`
+  spanAudioCurrTime.innerHTML = time
+
+  progressBar.value = audio.currentTime
+})
+
+progressBar.addEventListener('input', () => {
+audio.currentTime = progressBar.value
+})
+
+
+
+
+
+
+
+
+
+
